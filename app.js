253d2823da407d6d42f4e95d63320af67f4f21f2
fiamanillah <fiamanillahb2@gmail.com>
2025-01-14 02:29:42 +0600

    Upload

--- app.js
+++ app.js
@@ -12,7 +12,13 @@ require('./config/mongo');
 // Express setup
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+    cors({
+        origin: '*', // Specify the frontend URL (localhost or production URL)
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+        allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+    })
+);
 
 // Routes
 app.use('/api', formRoutes);
