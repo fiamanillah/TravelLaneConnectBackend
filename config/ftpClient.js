b023e62b979be21bfdac02aa77d582280a197cd0
fiamanillah <fiamanillahb2@gmail.com>
2025-01-16 00:44:42 +0600

    Upload

--- config/ftpClient.js
+++ config/ftpClient.js
@@ -2,6 +2,18 @@ const ftp = require('ftp');
 
 const ftpClient = new ftp();
 
+// Function to reconnect with a delay after the connection is closed
+const reconnectFtp = () => {
+    console.log('FTP connection closed, reconnecting...');
+    setTimeout(() => {
+        ftpClient.connect({
+            host: process.env.FTP_HOST,
+            user: process.env.FTP_USER,
+            password: process.env.FTP_PASSWORD,
+        });
+    }, 5000); // Delay of 5 seconds before reconnecting
+};
+
 ftpClient.on('ready', () => {
     console.log('FTP client connected');
 });
@@ -10,6 +22,9 @@ ftpClient.connect({
     host: process.env.FTP_HOST,
     user: process.env.FTP_USER,
     password: process.env.FTP_PASSWORD,
+    keepalive: 600000, // Keeps the connection alive by sending a NOOP command every 10 minutes
 });
+
+ftpClient.on('close', reconnectFtp);
 
 module.exports = ftpClient;
